14eea4bf05102d4c5fecb6f8f930aae189670d76
Deoh <darylhyde_13@hotmail.com>
2020-08-10 19:45:11 +0000

    Fix bugs for the gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     // check to see if username is entered
@@ -71,3 +73,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
